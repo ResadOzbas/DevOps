a121f66dd7b2515a0a22e27d86b8819dfa2a2457
Resad <resad.ozbas@outlook.com>
2024-05-07 23:34:21 +0100

    start of tasks1

---
+++ cs50/Tasks1/deep/deep.py
@@ -0,0 +1,7 @@
+user_input = input(
+    'What is the Answer to the Great Question of Life, the Universe, and Everything? ').lower()
+
+if user_input == '42' or user_input == 'forty-two' or user_input == 'forty two':
+    print('Yes')
+else:
+    print('No')
